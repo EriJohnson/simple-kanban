71866df94471d41a1bd47d74d4fb3f0391a13289
Eri JS <erijohnsons.fernandes@gmail.com>
2023-08-12 10:28:28 -0300

    chore: absolute imports configs

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
-import theme from "./assets/theme.ts";
 import App from "./App.tsx";
+import theme from "./assets/theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
